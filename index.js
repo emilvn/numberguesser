9d870a3cbe40414f440154dd7509c9a97e3964cb
emilvn <emilvnielsen@hotmail.com>
2024-09-06 15:10:41 +0200

    implemented binary search

--- index.js
+++ index.js
@@ -6,62 +6,115 @@ let maxNum = 100;
 let previousGuesses = [];
 
 function main() {
-    const lowerButton = document.querySelector("#lower");
-    const higherButton = document.querySelector("#higher");
-    const correctButton = document.querySelector("#correct");
+  const lowerButton = document.querySelector("#lower");
+  const higherButton = document.querySelector("#higher");
+  const correctButton = document.querySelector("#correct");
 
-    makeGuess(previousGuesses);
+  makeGuess(previousGuesses);
 
-    lowerButton.addEventListener("click", lower)
-    higherButton.addEventListener("click", higher)
-    correctButton.addEventListener("click", correct)
+  lowerButton.addEventListener("click", lower);
+  higherButton.addEventListener("click", higher);
+  correctButton.addEventListener("click", correct);
 }
 
 function makeGuess() {
-    const guessContainer = document.querySelector("#guesses");
-    const currentGuessElement = document.querySelector("#current-guess")
-    const previousGuess = currentGuessElement.innerText !== "" ? Number(currentGuessElement.innerText) : null
+  if (minNum > maxNum) {
+    return;
+  }
 
-    if (previousGuess) {
-        previousGuesses.push(previousGuess)
-        guessContainer.insertAdjacentHTML("beforeend", `<li class="guess">Previous guess: ${previousGuess}</li>`)
-    }
-    
-    let newGuess = Math.ceil(Math.random() * (maxNum - minNum) + minNum); // Get random number in range between max and min
-    while (previousGuesses.includes(newGuess)) {
-        if (maxNum - minNum < 2) break;
-        newGuess = Math.ceil(Math.random() * (maxNum - minNum) + minNum);
-    }
-    console.log(newGuess);
-    
+  // Get number halfway between max and min
+  let newGuess = Math.floor((maxNum + minNum) / 2);
 
-    currentGuessElement.innerText = newGuess;
-    return newGuess;
+  const guessContainer = document.querySelector("#guesses");
+  const currentGuessElement = document.querySelector("#current-guess");
+
+  const previousGuess =
+    currentGuessElement.innerText !== ""
+      ? Number(currentGuessElement.innerText)
+      : null;
+
+  if (previousGuess && previousGuess !== newGuess) {
+    previousGuesses.push(previousGuess);
+    guessContainer.insertAdjacentHTML(
+      "beforeend",
+      `<li class="guess">Previous guess: ${previousGuess} ${
+        previousGuess < newGuess ? "⬆️" : "⬇️"
+      }</li>`
+    );
+  }
+
+  currentGuessElement.innerText = newGuess;
+  return newGuess;
 }
 
 function higher() {
-    const currentGuessElement = document.querySelector("#current-guess")
-    const currentGuess = Number(currentGuessElement.innerText);
-    minNum = currentGuess;
-    makeGuess();
+  const currentGuessElement = document.querySelector("#current-guess");
+  const currentGuess = Number(currentGuessElement.innerText);
+  minNum = currentGuess + 1;
+  makeGuess();
 }
 
 function lower() {
-    const currentGuessElement = document.querySelector("#current-guess")
-    const currentGuess = Number(currentGuessElement.innerText);
-    maxNum = currentGuess;
-    makeGuess();
+  const currentGuessElement = document.querySelector("#current-guess");
+  const currentGuess = Number(currentGuessElement.innerText);
+  maxNum = currentGuess - 1;
+  makeGuess();
 }
 
 function correct() {
-    const guessContainer = document.querySelector("#guesses");
-    const currentGuessElement = document.querySelector("#current-guess")
-    
-    minNum = 0;
-    maxNum = 100;
-    previousGuesses = [];
+  const guessContainer = document.querySelector("#guesses");
+  const restartButton = document.querySelector("#restart");
+  const lowerButton = document.querySelector("#lower");
+  const higherButton = document.querySelector("#higher");
+  const correctButton = document.querySelector("#correct");
+  const guesses = previousGuesses.length + 1;
 
-    guessContainer.innerHTML = ""
-    currentGuessElement.innerText = ""
-    makeGuess();
+  guessContainer.innerText = `I got it in ${
+    guesses === 1 ? "1 guess" : `${guesses} guesses`
+  }. ${getCelebrationText()}`;
+
+  restartButton.addEventListener("click", restart);
+  restartButton.style.display = "block";
+  lowerButton.style.display = "none";
+  higherButton.style.display = "none";
+  correctButton.style.display = "none";
+}
+
+function getCelebrationText() {
+  switch (previousGuesses.length) {
+    case 0:
+      return "I'm a genius!";
+    case 1:
+    case 2:
+    case 3:
+      return "I'm pretty good at this!";
+    case 4:
+    case 5:
+      return "Meh.";
+    default:
+      return "I'm not very good at this.";
+  }
+}
+
+function restart() {
+  const restartButton = document.querySelector("#restart");
+  restartButton.removeEventListener("click", restart);
+  const guessContainer = document.querySelector("#guesses");
+  const currentGuessElement = document.querySelector("#current-guess");
+  const lowerButton = document.querySelector("#lower");
+  const higherButton = document.querySelector("#higher");
+  const correctButton = document.querySelector("#correct");
+
+  restartButton.style.display = "none";
+  lowerButton.style.display = "block";
+  higherButton.style.display = "block";
+  correctButton.style.display = "block";
+
+  minNum = 0;
+  maxNum = 100;
+  previousGuesses = [];
+
+  guessContainer.innerHTML = "";
+  currentGuessElement.innerText = "";
+  makeGuess();
 }
